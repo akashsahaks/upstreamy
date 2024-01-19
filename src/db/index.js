720b857622ca4ac388a65e128ba746f53a47d407
akash-sah-aks <akashsah.aks@gmail.com>
2024-01-19 19:12:27 +0530

    improved  connectDB code and created connection to mongodb, and create express  app in app.js and finally wrote code to start server

--- src/db/index.js
+++ src/db/index.js
@@ -11,7 +11,6 @@ const connectDB = async () => {
       );
    } catch (error) {
       console.log("MONGODB connection FAILED : ", error);
-      process.exit(1);
    }
 };
 
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-// require("dotenv").config({ path: "./env" });
 import "dotenv/config";
-
-import express from "express";
 import connectDB from "./db/index.js";
-const app = express();
+import app from "./app.js";
+const PORT = process.env.PORT || 8000;
 
-connectDB();
+connectDB()
+   .then(() => {
+      app.listen(PORT, () => {
+         console.log(`Server is running at port ${PORT}`);
+      }),
+         app.on("error", (error) => {
+            console.error("ERROR: ", error);
+            throw error;
+         });
+   })
+   .catch((err) => {
+      console.log("MONGO DB connection error: " + err);
+      process.exit(1);
+   });
